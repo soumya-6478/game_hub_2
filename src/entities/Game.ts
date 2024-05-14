5bc439d33bdedd12921977377ff2a9396b8ee4c7
Soumya Ranjan <soumya.6478@gmail.com>
2024-05-14 12:33:29 +0530

    build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,9 +1,13 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
   description_raw: string;
+  genres: Genre[];
+  publishers: Publisher[];
   slug: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
