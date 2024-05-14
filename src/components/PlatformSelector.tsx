1272a899a934aa16c911c3dc53730c3dbc77643d
Soumya Ranjan <soumya.6478@gmail.com>
2024-05-14 14:07:31 +0530

    refactor entity imports

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,6 +1,6 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 import usePlatforms from "../hooks/usePlatforms";
 import usePlatform from "../hooks/usePlatform";
 import useGameQueryStore from "../store";
